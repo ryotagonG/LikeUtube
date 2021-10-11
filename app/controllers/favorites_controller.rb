46ad650c21b5510b9970775e770a5520c87e9ced
ryotagonG <ryotapb715@gmail.com>
2021-10-11 05:48:50 +0000

    いいね機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+  
+  def create
+    article = Article.find(params[:article_id])
+    favorite = current_user.favorites.new(article_id: article.id)
+    favorite.save
+    redirect_to article_path(article)
+  end
+  
+  def destroy
+    article = Article.find(params[:article_id])
+    favorite = current_user.favorites.find_by(article_id: article.id)
+    favorite.destroy
+    redirect_to article_path(article)
+  end
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
